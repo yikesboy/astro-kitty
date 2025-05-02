ff51d0fd2209f68470f303c30d1ad3f1893898c3
JL <115737916+yikesboy@users.noreply.github.com>
2025-05-02 11:54:29 +0200

    feat: prototype win eval

--- scripts/entities/rocket.gd
+++ scripts/entities/rocket.gd
@@ -5,13 +5,17 @@ extends RigidBody2D
 var thrust_force: float = 150.0
 var rotation_speed: float = 100.0
 var gravity: float = 40.0
-var landing_speed_threshold: float = 50.0
+var landing_speed_threshold: float = 100.0
 var max_speed: float = 300.0
 
 var is_thrusting: bool = false
 var thrust_started: bool = false
 var thrust_start_time: float = 0.0
 const thrust_buil_duration: float = 1.0
+
+var is_touching_platform: bool = false
+var contact_start_time: float = 0.0
+var steady_landing_duration: float = 2.0
 
 func _ready() -> void:
 	$RocketAnimatedSprite.play("landing")
@@ -61,10 +65,39 @@ func _physics_process(delta: float) -> void:
 	if linear_velocity.length() > max_speed:
 		linear_velocity = linear_velocity.normalized() * max_speed
 		
+	if is_touching_platform:
+		var speed = linear_velocity.length()
+		var is_slow_enough = speed < landing_speed_threshold
+		
+		if is_slow_enough:
+			var elapsed = Time.get_ticks_msec() / 1000.0 - contact_start_time
+			if elapsed >= steady_landing_duration:
+				Win()
+		else:
+			is_touching_platform = false
+		
 func _on_body_entered(body) -> void:
 	print("Collided with: ", body.name)
 	if body.name.begins_with("Enemy"):
 		Kill()
+	elif body.name == "Goal":
+		var speed = linear_velocity.length()
+		var is_slow_enough = speed < landing_speed_threshold
+				
+		if is_slow_enough:
+			is_touching_platform = true
+			contact_start_time = Time.get_ticks_msec() / 1000.0
+		else:
+			print(is_slow_enough)
+			Kill()
+
+func _on_body_exited(body) -> void:
+	if body.name == "Goal":
+		is_touching_platform = false
+		
+
+func Win():
+	print("WIN")
 
 func Kill():
 	var _particle = deathParticle.instantiate()
